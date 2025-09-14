23a03bc5fc8a0810cfb9446a3eead82ddbcafe08
Егор Романович <Egor_rom13@mail.ru>
2025-09-14 12:10:28 +0500

    Task3

---
+++ Tyuiu.RomanovichEN.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.RomanovichEN.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }   
+}
---
+++ Tyuiu.RomanovichEN.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RomanovichEN.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RomanovichEN.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e9fca36-12c4-46dd-a784-5e55dfc260ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
